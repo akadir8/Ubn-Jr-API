2c9ff6d636cdfb57d7d1327b6d5fc61712c2376b
akadir8 <bickiabdulkadir12@gmail.com>
2023-05-05 11:39:20 +0300

    data API

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.post('/api/form', (req, res) => {
+  const formData = req.body;
+  console.log(formData);
+
+  // process form data here
+
+  res.send('Form data received!');
+});
+
+app.listen(5000, () => {
+  console.log('Server listening on port 5000');
+});
